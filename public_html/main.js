a66ecd455c8e06e5750898e7920770f01e8b73ce
Raymond Fallon <raymondfallon@gmail.com>
2017-08-14 19:14:23 -0400

    Sanitized user input. Added Superhero story.

--- public_html/main.js
+++ public_html/main.js
@@ -4,9 +4,46 @@ var finishedStory = '';
 var prompts = {
   'ray': ['First off, what\'s your company name? I mean...you\'re not reading \n\
   this for fun, are you?', 'Adjective', 'Adverb', 'Adjective', 'Plural noun',
-  'A number (How \'bout a positive integer?)', 'An article of clothing', 'Body part']
+  'A number (How \'bout a positive integer?)', 'An article of clothing', 'Body part'],
+  'hero': ['We\'re going to need some characters, right? Let\'s start with \n\
+  a boy\'s name', 'Now balance the scales with a girl\s name', 'Adjective', 
+  'Verb: present participle (that\'s those "ing" ones)', 'An animal', 'Adjective', 
+  'An element (natural or periodic...whichever you\'re feeling!)', 'A hobby...\n\
+  one of yours, if you want! No pressure.']
 };
+var lastNames = {
+  'a': "Allen",
+  'b': "Barker",
+  'c': "Crocker",
+  'd': "DeLaney",
+  'e': "Everett",
+  'f': "Faust",
+  'g': "Gillibrand",
+  'h': "Hill",
+  'i': "Ignacious",
+  'j': "Jacobi",
+  'k': "Kilgore",
+  'l': "LeBuff",
+  'm': "McGruff",
+  'n': "Nixon",
+  'o': "Oolong",
+  'p': "Parquette",
+  'q': "Quest",
+  'r': "Roughshot",
+  's': "Shade",
+  't': "Turner",
+  'u': "Umber",
+  'v': "Viego",
+  'w': "Wallace",
+  'x': "Xanthos",
+  'y': "Yarbley",
+  'z': "Ziggarello",
+ }
 var promptIndex = 0;
+
+function cleanString(s) {
+    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
+}
 
 function build_story_from_input() {
   $(document).prop('title', 'test0');
@@ -18,24 +55,20 @@ function build_story_from_input() {
       if (/^[aeiouAEIOU]/i.test(userInputs[1])) {a1 = 'an ';}
       if (/^[aeiouAEIOU]/i.test(userInputs[3])) {a2 = 'an ';}
       if (/^[aeiouAEIOU]/i.test(userInputs[6])) {a3 = 'an ';}
-      //if (userInputs[1][0] === /^[aeiou]/i){a1 = 'an ';}
-      //if (userInputs[3][0] === /[aeiou]/i){a2 = 'an ';}
-      //if (userInputs[6][0] === /[aeiou]/i){a3 = 'an ';}
       if (/s$/i.test(userInputs[6])) {a3 = '';}
       $(document).prop('title', 'testinX');
-      //return "hi" + a1 + a2 + userInputs[7] + "hiii";
-      return "Greetings! My name is Ray Fallon. I am " + a1 + userInputs[1] + 
-        " software developer, eager to make my professional start in the \n\
-        industry. I'd like to say upfront and without hyperbole that I think " 
-        + userInputs[0] + " is the most exciting company in its field \n\
-        and is definitely my first choice, bar none. But you " + 
-        userInputs[2] + " want to know a little more about me first! \n\
-        Well, I'm from Haddon Township, " + a2 + userInputs[3] + " little \n\
-        suburb of NJ, affectionately known for its antique " + userInputs[4] + 
-        ". I'm the youngest of " + userInputs[5] + " children, which \n\
+      return "Greetings! My name is Ray Fallon. I am " + a1 + 
+        userInputs[1].toLowerCase() + " software developer, eager to make my \n\
+        professional start in the industry. I'd like to say upfront and \n\
+        without hyperbole that I think " + userInputs[0] + " is the most \n\
+exciting company in its field and is definitely my first choice, bar none. \n\
+But you " + userInputs[2].toLowerCase() + " want to know a little more about me first! \n\
+        Well, I'm from Haddon Township, " + a2 + userInputs[3].toLowerCase() + " little \n\
+        suburb of NJ, affectionately known for its antique " + userInputs[4].toLowerCase() + 
+        ". I'm the youngest of " + userInputs[5].toLowerCase() + " children, which \n\
         probably tells you all you need to know!  HAHA, am I right \n\
         HR-person? Anyway, as long as your developers don't have to wear " +  
-        a3 + userInputs[6] + " to work, I think we've got a match! So shoot \n\
+        a3 + userInputs[6].toLowerCase() + " to work, I think we've got a match! So shoot \n\
         me an email at Raymond.Fallon@" + userInputs[0].toLowerCase() + 
         ".com and we'll get the ball rolling!  Heh, just kidding, email me \n\
         at " + userInputs[1].toLowerCase() + "-Ray@" + 
@@ -43,6 +76,72 @@ function build_story_from_input() {
         (Let me know if your insurance covers Vision or if it's just Dental \n\
         and " + userInputs[7][0].toUpperCase()  + 
         userInputs[7].slice(1).toLowerCase() + " coverage.)";
+    case 'hero':
+      var boyFirst, boyLast, girlFirst, girlLast, he_lower, He_upper, 
+              a1, manGood, firstNameGood, lastNameGood, manEvil, firstNameEvil,
+              lastNameEvil, animalUpper, elementUpper;
+      try {
+        boyFirst = userInputs[0][0].toUpperCase() + 
+                userInputs[0].slice(1).toLowerCase();
+        boyLast = lastNames[userInputs[0][0].toLowerCase()];
+        girlFirst = userInputs[1][0].toUpperCase() + 
+                userInputs[1].slice(1).toLowerCase();
+        girlLast = lastNames[userInputs[1][0].toLowerCase()];
+        animalUpper = userInputs[4][0].toUpperCase() + 
+                userInputs[4].slice(1).toLowerCase();
+        elementUpper = userInputs[6][0].toUpperCase() + 
+                userInputs[6].slice(1).toLowerCase();
+      }
+      catch(err) {
+        boyFirst = userInputs[0];
+        boyLast = "Parker";
+        girlFirst = userInputs[1];
+        girlLast = "Wallace";
+        animalUpper = userInputs[4];
+        elementUpper = userInputs[6];
+      }
+      if (Math.random() > 0.5) {
+        // the boy is the hero
+        he_lower = 'he';
+        He_upper = 'He';
+        manGood = '-Man';
+        manEvil = '-Woman';
+        firstNameGood = boyFirst;
+        lastNameGood = boyLast;
+        firstNameEvil = girlFirst;
+        lastNameEvil = girlLast;
+      } else {
+        // the girl is the hero
+        he_lower = 'she';
+        He_upper = 'She';
+        manGood = '-Woman';
+        manEvil = '-Man';
+        firstNameGood = girlFirst;
+        lastNameGood = girlLast;
+        firstNameEvil = boyFirst;
+        lastNameEvil = boyLast;
+      }
+      if (/^[aeiouAEIOU]/i.test(userInputs[5])) {a1 = 'an ';}
+      else {a1 = 'a ';}
+      
+      return "It was just a normal, " + userInputs[2] + " day for " +
+firstNameGood + " " + lastNameGood + ". " + He_upper + " was just " + 
+userInputs[3] + " in the science lab when " + he_lower + " was suddenly \n\
+bitten by a radioactive " + userInputs[4].toLowerCase() + "! After some slight\n\
+soreness, followed by a 3-day coma, followed by " + a1 + userInputs[5] + 
+"costume montage, " + firstNameGood + " became the mighty " + animalUpper +
+manGood + "! But " + animalUpper + manGood + " wasn't the only wonder to come \n\
+out of that science lab--oh no! It was there that a freak " + 
+userInputs[6].toLowerCase() + " accident turned " + firstNameEvil + " " + lastNameEvil +
+" into the infamous and fearsome " + elementUpper + manEvil + ". " +
+elementUpper + manEvil + " was every inch the villain that " + animalUpper + 
+manGood + " was the hero. The two were polar opposites--except for their \n\
+mutual love of " + userInputs[7].toLowerCase() + "! But can " + animalUpper +
+manGood + " harness this " + userInputs[7].toLowerCase() + "-love to turn \n\
+his rival to the right side of the law? Or will their rivalry raze this city \n\
+to dust? Find out this summer in: " + firstNameGood + " " + lastNameGood + 
+" v. " + firstNameEvil + " " + lastNameEvil + ": The Birth of the " + 
+elementUpper + "-" + animalUpper + ".";
     default:
       $(document).prop('title', 'testinY');
       return 'whoops';
@@ -50,7 +149,10 @@ function build_story_from_input() {
 }
 
 function try_input(phrase) {
-    /* TODO Remove empty strings here */
+    // TODO Remove empty strings here 
+    phrase = cleanString(phrase);
+    no_space_phrase = phrase.replace(/\s*/g, '');
+    if (no_space_phrase === ''){$('#user_in').val(''); return false;}
     userInputs.push(phrase);
     if (userInputs.length > 7) {
         $('#zany_build_page').css('display', 'none');
@@ -74,12 +176,14 @@ function main() {
     $(this).next().slideToggle(100);
   });
   $('.wel_select').mouseenter(function(){
+    $(this).css('cursor', 'pointer');
     $(this).css('background-color', 'yellow');
   });
   $('.wel_select').mouseleave(function(){
     $(this).css('background-color', 'transparent');
   });
   $('.wel_opt').mouseenter(function(){
+    $(this).css('cursor', 'pointer');
     $(this).css('font-size', '20px');
   });
   $('.wel_opt').mouseleave(function(){
@@ -106,8 +210,8 @@ function main() {
       case 'Zoo Story':
         storyChosen = 'zoo';
         break;
-      case 'Jet-setting':
-        storyChosen = 'jet';
+      case 'The Hero\'s Journey':
+        storyChosen = 'hero';
         break;
       default:
         storyChosen = 'oh jeez...something\'s gone wrong.';
